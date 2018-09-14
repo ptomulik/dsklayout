a3c0b35a2e6cc76424b1508f07568db500a1f631
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2018-09-14 12:04:56 +0200

    2018.09.14 12:04

--- lib/dsklayout/cli/__init__.py
+++ lib/dsklayout/cli/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf8 -*-
 """Command-line implementation.
 
-This module implements dsklayout command-line interface. The entry point for
-dsklayout CLI is the :func:`.main` function, which can be conveniently used
-in `setup.py`.
+This module implements dsklayout command-line interface. It provides an entry
+point for dsklayout application -- the :func:`.main` function. The function can
+be conveniently used with :func:`setuptools.setup`.
+
+There are also classes that help implementing custom CLI applications with
+subcommands and flags/options. At the top, there is :class:`.CliApp` class,
+which is a base class for CLI applications (any new CLI application shall
+be implemented as a subclass of :class:`CliApp`).
 """
 
 from .. import util
