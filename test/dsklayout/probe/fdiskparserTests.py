957abcbcdcb74be5777c6457edc2105ec1365092
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2018-09-28 08:21:35 +0200

    2018.09.28 08:21

--- test/dsklayout/probe/fdiskparserTests.py
+++ test/dsklayout/probe/fdiskparserTests.py
@@ -46,34 +46,12 @@ class Test__FdiskParser(testcase_.ProbeTestCase):
     def test__adjust_right__1(self):
         #          00000000001111
         #          01234567890123
+        #          .  |<
         pattern = "*** **** *****"
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 1, None), 3)
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 2, None), 3)
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 3, None), 3)
 
-    def test__adjust_right__2(self):
-        #          00000000001111
-        #          01234567890123
-        pattern = "*** **** *****"
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 5, None), 8)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 6, None), 8)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 7, None), 8)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 8, None), 8)
-
-    def test__adjust_right__3(self):
-        #          00000000001111
-        #          01234567890123
-        pattern = "*** **** *****"
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 10, None), 14)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 11, None), 14)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 12, None), 14)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 13, None), 14)
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 14, None), 14)
-
-    def test__adjust_right__4(self):
-        #          00000000001111
-        #          01234567890123
-        pattern = "*** **** *****"
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 1, 1, None), 3)
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 1, 2, None), 3)
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 1, 3, None), 3)
@@ -82,11 +60,182 @@ class Test__FdiskParser(testcase_.ProbeTestCase):
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 2, 2, None), 3)
         self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 2, 3, None), 3)
 
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 7, None), 7)
+
+    def test__adjust_right__2(self):
+        #          00000000001111
+        #          01234567890123
+        #              .   |<
+        pattern = "*** **** *****"
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 5, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 6, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 7, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 4, 8, None), 8)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 5, 5, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 5, 6, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 5, 7, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 5, 8, None), 8)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 6, 5, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 6, 6, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 6, 7, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 6, 8, None), 8)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 7, 5, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 7, 6, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 7, 7, None), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 7, 8, None), 8)
+
+    def test__adjust_right__3(self):
+        #          00000000001111
+        #          01234567890123
+        #                   .    |<
+        pattern = "*** **** *****"
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 10, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 11, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 12, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 13, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 9, 14, None), 14)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 10, 10, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 10, 11, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 10, 12, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 10, 13, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 10, 14, None), 14)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 11, 10, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 11, 11, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 11, 12, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 11, 13, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 11, 14, None), 14)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 12, 10, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 12, 11, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 12, 12, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 12, 13, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 12, 14, None), 14)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 13, 10, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 13, 11, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 13, 12, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 13, 13, None), 14)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 13, 14, None), 14)
+
+    def test__adjust_right__4(self):
+        #          00000000001111
+        #          01234567890123
+        #          .  |<
+        pattern = "*** **** *****"
+        rng = {'B': (4, 8), 'C': (8, 14)}
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 2, rng), 3)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 3, rng), 3)
+
     def test__adjust_right__5(self):
         #          00000000001111
         #          01234567890123
+        #          .       |<
+        pattern = "*******  *****"
+        rng = {'C': (9, 14)}
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 2, rng), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 3, rng), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 0, 10, rng), 14)
+
+    def test__adjust_left__1(self):
+        #          00000000001111
+        #          01234567890123
+        #         >|  .
         pattern = "*** **** *****"
-        self.assertEqual(fdiskparser_.FdiskParser._adjust_right(pattern, 1, 7, None), 7)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 0, 1, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 0, 2, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 0, 3, None), 0)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 1, 1, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 1, 2, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 1, 3, None), 0)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 2, 1, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 2, 2, None), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 2, 3, None), 0)
+
+    def test__adjust_left__2(self):
+        #          00000000001111
+        #          01234567890123
+        #             >|   .
+        pattern = "*** **** *****"
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 4, 5, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 4, 6, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 4, 7, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 4, 8, None), 4)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 5, 5, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 5, 6, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 5, 7, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 5, 8, None), 4)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 6, 5, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 6, 6, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 6, 7, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 6, 8, None), 4)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 7, 5, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 7, 6, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 7, 7, None), 4)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 7, 8, None), 4)
+
+    def test__adjust_left__3(self):
+        #          00000000001111
+        #          01234567890123
+        #                  >|    .
+        pattern = "*** **** *****"
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 10, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 11, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 12, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 13, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 14, None), 9)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 10, 10, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 10, 11, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 10, 12, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 10, 13, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 10, 14, None), 9)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 11, 10, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 11, 11, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 11, 12, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 11, 13, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 11, 14, None), 9)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 10, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 11, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 12, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 13, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 14, None), 9)
+
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 13, 10, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 13, 11, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 13, 12, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 13, 13, None), 9)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 13, 14, None), 9)
+
+    def test__adjust_left__4(self):
+        #          00000000001111
+        #          01234567890123
+        #         >|  .
+        pattern = "*** **** *****"
+        rng = {'B': (4, 8), 'C': (8, 14)}
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 0, 2, rng), 0)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 0, 3, rng), 0)
+
+    def test__adjust_left__5(self):
+        #          00000000001111
+        #          01234567890123
+        #          (      )|<
+        pattern = "*******  *****"
+        rng = {'A': (0, 7)}
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 9, 14, rng), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 12, 14, rng), 8)
+        self.assertEqual(fdiskparser_.FdiskParser._adjust_left(pattern, 7, 10, rng), 0)
 
 if __name__ == '__main__':
     unittest.main()
